0a2517c33aa5733300bc5b9d2bb1dc32d858d89b
MaximKharin <maximkharin@gmail.com>
2022-06-20 13:47:17 +0300

    finished task3

---
+++ task3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 3: Напишите программу, которая принимает на вход число (N)
+//и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27. 
+//5 -> 1, 8, 27, 64, 125
+int num = prompt("Enter a number, please: ");
+for (int i = 1; i <= num; i++)
+{
+    Console.WriteLine($"{i * i * i}");
+}
+
+int prompt(string message)
+{
+    Console.WriteLine(message);
+    string number = Console.ReadLine();
+    int numInt = int.Parse(number);
+    return numInt;
+}
